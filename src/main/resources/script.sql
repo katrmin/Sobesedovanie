c21f8bd3169c8f0c5f47c75c84c2d377d9130d18
katrmin <katrmin@yandex.ru>
2021-06-07 20:14:41 +0300

    Поправлен черновой вариант создания таблиц

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,5 +1,5 @@
 create table  Film(
-    id numeric primary key AUTO_INCREMENT,
+    id decimal primary key AUTO_INCREMENT,
     name  varchar (1000),
     duration int,
     id_ticket numeric,
@@ -7,17 +7,17 @@ create table  Film(
     REFERENCES (Ticket(id))
 );
 create table Ticket(
-    id numeric primary key AUTO_INCREMENT,
+    id decimal primary key AUTO_INCREMENT,
     number int,
     id_session numeric ,
     foreign key(id_session) ,
     references (SESSION (id))
 );
 create table Session(
-    id  numeric  primary key AUTO_INCREMENT,
-    time DATE,
+    id  decimal  primary key AUTO_INCREMENT,
+    time TIMESTAMP ,
     id_ticket numeric,
-    price numeric ,
+    price DECIMAL(7,2) ,
     foreign key(id_ticket),
     REFERENCES (Ticket(id))
 );
